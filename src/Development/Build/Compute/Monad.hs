63790751df8c377b8e3a2b04f62f77c93c296b1a
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-03-04 12:36:33 +0000

    Minor renaming

--- src/Development/Build/Compute/Monad.hs
+++ src/Development/Build/Compute/Monad.hs
@@ -52,10 +52,10 @@ staticDependencies :: (forall f. Monad f => Compute f k v) -> k -> [k]
 staticDependencies compute = staticScriptDependencies . getScript compute
 
 data Script k v a where
-    GetValue :: k -> Script k v v
-    Pure     :: a -> Script k v a
-    Ap       :: Script k v (a -> b) -> Script k v a -> Script k v b
-    Bind     :: Script k v a -> (a -> Script k v b) -> Script k v b
+    Get  :: k -> Script k v v
+    Pure :: a -> Script k v a
+    Ap   :: Script k v (a -> b) -> Script k v a -> Script k v b
+    Bind :: Script k v a -> (a -> Script k v b) -> Script k v b
 
 instance Functor (Script k v) where
     fmap = Ap . Pure
@@ -70,33 +70,33 @@ instance Monad (Script k v) where
     (>>=)  = Bind
 
 getScript :: (forall f. Monad f => Compute f k v) -> k -> Script k v (Maybe v)
-getScript compute = compute GetValue
+getScript compute = compute Get
 
 runScript :: Monad m => (k -> m v) -> Script k v a -> m a
 runScript get script = case script of
-    GetValue k -> get k
-    Pure v     -> pure v
-    Ap s1 s2   -> runScript get s1 <*> runScript get s2
-    Bind s f   -> runScript get s >>= fmap (runScript get) f
+    Get k    -> get k
+    Pure v   -> pure v
+    Ap s1 s2 -> runScript get s1 <*> runScript get s2
+    Bind s f -> runScript get s >>= fmap (runScript get) f
 
 -- TODO: Fix inifinite loop
 staticScriptDependencies :: Script k v a -> [k]
 staticScriptDependencies script = case script of
-    GetValue k -> [k]
-    Pure _     -> []
-    Ap s1 s2   -> staticScriptDependencies s1 ++ staticScriptDependencies s2
-    Bind s _   -> staticScriptDependencies s
+    Get k    -> [k]
+    Pure _   -> []
+    Ap s1 s2 -> staticScriptDependencies s1 ++ staticScriptDependencies s2
+    Bind s _ -> staticScriptDependencies s
 
 isStatic :: Script k v a -> Bool
 isStatic script = case script of
-    GetValue _ -> True
-    Pure _     -> True
-    Ap s1 s2   -> isStatic s1 && isStatic s2
-    Bind _ _   -> False
+    Get _    -> True
+    Pure _   -> True
+    Ap s1 s2 -> isStatic s1 && isStatic s2
+    Bind _ _ -> False
 
 isInput :: Eq k => Script k v a -> k -> Bool
 isInput script key = case script of
-    GetValue k -> k == key
-    Pure _     -> True
-    Ap s1 s2   -> isInput s1 key && isInput s2 key
-    Bind _ _   -> False
+    Get k    -> k == key
+    Pure _   -> True
+    Ap s1 s2 -> isInput s1 key && isInput s2 key
+    Bind _ _ -> False
