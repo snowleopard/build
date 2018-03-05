92cef6ed9b38e057ea1ba9aa4be5cdc6ac4d5902
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-03-05 03:20:20 +0000

    Add correctness and idempotence properties for Build

--- src/Development/Build.hs
+++ src/Development/Build.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE ConstraintKinds, RankNTypes, ScopedTypeVariables #-}
 module Development.Build (
     -- * Build
-    Build, PureBuild, purify, dumbBuild, dumbTracingBuild, slowBuild
+    Build, PureBuild, purify, dumbBuild, dumbTracingBuild, slowBuild,
 
     -- * Properties
-    -- consistent, correct, idempotent
+    correct, idempotent
     ) where
 
 import Control.Monad.IO.Class
@@ -14,29 +14,18 @@ import Data.Functor.Identity
 import Development.Build.Compute
 import Development.Build.Compute.Monad
 import Development.Build.Store
-
--- | Check a three-way consistency between a 'Compute' function, a 'Plan' and
--- a 'Store' with respect to a given key. This involves checking the following:
--- * The plan is complete, i.e. all dependencies of the key are known.
--- * The ('Plan', 'Store') pair agrees with the 'Compute' function.
--- consistent :: (Eq v, Monad m, GetHash m k v) => Compute Monad k v -> Plan k v -> k -> m Bool
--- consistent compute plan key = case plan key of
---     Nothing -> return False -- The plan is incomplete
---     Just (h, deps) -> do
---         h' <- getHash key
---         vc <- compute key
---         vs <- getValue key
---         cs <- mapM (consistent compute plan . fst) deps
---         return $ h' == h && vc == vs && and cs
+import Development.Build.Utilities
 
 -- | A build system takes a 'Compute', a list of output keys, an initial state
 -- of type @s@, and builds the outputs, producing the new state.
-type Build c m k v s = Store m k v => (forall f. c f => Compute f k v) -> [k] -> s -> m s
+type Build c m k v s =
+    Store m k v => (forall f. c f => Compute f k v) -> [k] -> s -> m s
 
-type PureBuild c k v s = (forall f. c f => Compute f k v) -> [k] -> s -> s
+type PureBuild c k v s =
+    (forall f. c f => Compute f k v) -> [k] -> (s, k -> v) -> (s, k -> v)
 
 -- How do I express that we can do it for any c, not just c = Monad?
-purify :: Eq k => (forall m. Build Monad m k v s) -> PureBuild Monad k v (s, k -> v)
+purify :: Eq k => (forall m. Build Monad m k v s) -> PureBuild Monad k v s
 purify build compute outputs (s, store) =
     runIdentity $ runPureStore (build compute outputs s) store
 
@@ -74,10 +63,25 @@ slowBuild compute outputs s = mapM build outputs $> s
             Just value -> putValue k value >> return value
             Nothing    -> getValue k
 
+-- | Given a @build@ and @compute@, check that for any key-value map describing
+-- the contents of a store @before@ the build system is executed to build a list
+-- of @outputs@, the map @after@ the build is a correct build outcome.
+-- Specifically, there must exist a @magic@ key-value map, such that:
+-- * @before@, @after@ and @magic@ agree on the values of all inputs.
+-- * @after@ and @magic@ agree on the values of all outputs.
+-- * @magic@ is 'consistent' with the @compute@.
+-- We assume that @compute@ is acyclic. If it is not, the function returns @True@.
+correct :: (Eq k, Eq v) => (forall m. Build Monad m k v s)
+                        -> (forall f. Monad f => Compute f k v) -> Bool
+correct build compute = forall $ \(outputs, store, s) ->
+    let (_, newStore) = (purify build) compute outputs (s, store)
+    in correctBuild compute store newStore outputs
+
 -- | Check that a build system is /idempotent/, i.e. running it once or twice in
--- a row leads to the same 'Plan' and 'Store'.
--- idempotent :: (Eq k, Eq v) => Store m k v => Build m k v -> m Bool
--- idempotent build = forallM $ \(compute, outputs, state, plan) -> do
---     (state', plan' ) <- build compute outputs (state , plan )
---     (_     , plan'') <- build compute outputs (state', plan')
---     return $ forall $ \key -> plan' key == plan'' key
+-- a row leads to the same resulting 'Store'.
+idempotent :: (Eq k, Eq v) => (forall m. Build Monad m k v s)
+                           -> (forall f. Monad f => Compute f k v) -> Bool
+idempotent build compute = forall $ \(outputs, store1, s1) ->
+    let (s2, store2) = (purify build) compute outputs (s1, store1)
+        (_ , store3) = (purify build) compute outputs (s2, store2)
+    in forall $ \key -> store2 key == store3 key
