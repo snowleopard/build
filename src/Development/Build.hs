4ed0b8fc3d9875f48e109e6ae2760645584a19d2
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-03-06 03:14:14 +0000

    Add MonadPlus, minor revision

--- src/Development/Build.hs
+++ src/Development/Build.hs
@@ -19,20 +19,20 @@ import Development.Build.Utilities
 import qualified Data.Map as Map
 
 -- | A build system takes a 'Compute', a list of output keys, some information
--- @i@ from the previous build (which can be missing if this is the first build),
+-- from the previous build @i@ (which can be missing if this is the first build),
 -- and a partial key-value map @Map k v@, and computes information for the next
 -- build and an updated key-value map. Note that we require @Ord k@ since one
 -- cannot do anything with 'Map' without it, but in principle @Eq k@ could be
 -- sufficient if we instead used @k -> Maybe v@ for key-value maps.
-type Build c k v i = Ord k => Compute c k v -> [k] -> Maybe i -> Map k v -> (i, Map k v)
+type Build c i k v = Ord k => Compute c k v -> [k] -> Maybe i -> Map k v -> (i, Map k v)
 
 type StoreBuild c m k v = Store m k v => Compute c k v -> [k] -> m ()
 
 -- How do I express that we can do it for any c, not just c = Monad?
-purify :: (k -> v) -> (forall m. StoreBuild Monad m k v) -> Build Monad k v ()
-purify def sbuild compute outputs _ = runMapStore (sbuild compute outputs) def
+purify :: (k -> v) -> (forall m. StoreBuild Monad m k v) -> Build Monad () k v
+purify def build compute outputs _ = runMapStore (build compute outputs) def
 
-dumb :: (k -> v) -> Build Monad k v ()
+dumb :: (k -> v) -> Build Monad () k v
 dumb def = purify def build
   where
     build compute outputs = mapM_ buildValue outputs
@@ -56,7 +56,7 @@ dumbTracing compute outputs = mapM_ build outputs
             Just value -> putValue k value
             Nothing    -> return ()
 
-slow :: (k -> v) -> Build Monad k v ()
+slow :: (k -> v) -> Build Monad () k v
 slow def = purify def build
   where
     build :: Store m k v => Compute Monad k v -> [k] -> m ()
@@ -78,7 +78,7 @@ slow def = purify def build
 -- * @after@ and @magic@ agree on the values of all outputs.
 -- * @magic@ is 'consistent' with the @compute@.
 -- We assume that @compute@ is acyclic. If it is not, the function returns @True@.
-correct :: (Ord k, Eq v) => Build Monad k v i -> Compute Monad k v -> Bool
+correct :: (Ord k, Eq v) => Build Monad i k v -> Compute Monad k v -> Bool
 correct build compute = forall $ \(outputs, store, i) ->
     let (_, newStore) = build compute outputs i store
     in correctBuild (partial compute) (total store) (total newStore) outputs
@@ -90,7 +90,7 @@ correct build compute = forall $ \(outputs, store, i) ->
 
 -- | Check that a build system is /idempotent/, i.e. running it once or twice in
 -- a row leads to the same resulting 'Store'.
-idempotent :: (Ord k, Eq v) => Build Monad k v i -> Compute Monad k v -> Bool
+idempotent :: (Ord k, Eq v) => Build Monad i k v -> Compute Monad k v -> Bool
 idempotent build compute = forall $ \(outputs, store1, s1) ->
     let (s2, store2) = build compute outputs (Just s1) store1
         (_ , store3) = build compute outputs (Just s2) store2
