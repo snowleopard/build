07593668220c9724e82ec1b237c22757a1d770f0
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-03-07 17:35:27 +0000

    Add Fibonacci example

--- src/Development/Build/Compute.hs
+++ src/Development/Build/Compute.hs
@@ -21,18 +21,31 @@ isInput compute = isNothing . compute (const Proxy)
 
 ----------------------------------- Examples -----------------------------------
 -- Collatz sequence:
--- a[0] = n
--- a[k] = f(a[k - 1]) where
+-- c[0] = n
+-- c[k] = f(c[k - 1]) where
 -- For example, if n = 12, one gets the sequence 12, 6, 3, 10, 5, 16, 8, 4, 2, 1.
 
 data Collatz = Collatz Int
 
 collatz :: Compute Functor Collatz Int
-collatz get (Collatz k) | k == 0    = Nothing
+collatz get (Collatz k) | k <= 0    = Nothing
                         | otherwise = Just $ f <$> get (Collatz (k - 1))
   where
     f n | even n    = n `div` 2
         | otherwise = 3 * n + 1
+
+-- Generalised Fibonacci sequence:
+-- f[0] = n
+-- f[1] = m
+-- f[k] = f[k - 1] + f[k - 1]
+-- For example, with (n, m) = (0, 1) we get usual Fibonacci sequence, and if
+-- (n, m) = (2, 1) we get Lucas sequence: 2, 1, 3, 4, 7, 11, 18, 29, 47 ...
+data Fibonacci = Fibonacci Int
+
+fibonacci :: Compute Applicative Fibonacci Int
+fibonacci get (Fibonacci k) | k <= 1    = Nothing
+                            | otherwise = Just $ (+) <$> get (Fibonacci (k - 1))
+                                                     <*> get (Fibonacci (k - 2))
 
 -- These type synonyms are not very useful, but enumerate all interesting cases.
 type FunctorialCompute  k v = forall f. Functor     f => (k -> f v) -> k -> Maybe (f v)
