feb14bde90e5109c0ac2ea86e5f261c085e607d8
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-03-07 16:56:50 +0000

    Add Collatz sequence example

--- src/Development/Build/Compute.hs
+++ src/Development/Build/Compute.hs
@@ -19,6 +19,21 @@ inputCompute _ _ = Nothing
 isInput :: Compute MonadPlus k v -> k -> Bool
 isInput compute = isNothing . compute (const Proxy)
 
+----------------------------------- Examples -----------------------------------
+-- Collatz sequence:
+-- a[0] = n
+-- a[k] = f(a[k - 1]) where
+-- For example, if n = 12, one gets the sequence 12, 6, 3, 10, 5, 16, 8, 4, 2, 1.
+
+data Collatz = Collatz Int
+
+collatz :: Compute Functor Collatz Int
+collatz get (Collatz k) | k == 0    = Nothing
+                        | otherwise = Just $ f <$> get (Collatz (k - 1))
+  where
+    f n | even n    = n `div` 2
+        | otherwise = 3 * n + 1
+
 -- These type synonyms are not very useful, but enumerate all interesting cases.
 type FunctorialCompute  k v = forall f. Functor     f => (k -> f v) -> k -> Maybe (f v)
 type ApplicativeCompute k v = forall f. Applicative f => (k -> f v) -> k -> Maybe (f v)
