dff8e11840605bff05b19c2d900100b3049f1099
Neil Mitchell <ndmitchell@gmail.com>
2018-03-16 23:11:39 +0000

    Add CloudShake

--- src/Development/Build.hs
+++ src/Development/Build.hs
@@ -234,3 +234,19 @@ bazel = topological $ \key ds act -> do
     else do
         when (getHash key s `notElem` poss) $
             modify $ putValue key (contents Map.! head poss)
+
+cloudShake :: (Eq k, Hashable v) => Build Monad (Traces k v) k v
+cloudShake = recursive $ \key fetch act -> do
+    s <- gets fst
+    let Traces traces contents = getInfo s
+    poss <- traceMatch (\k v -> (==) v . hash <$> fetch k) key traces
+    if null poss then do
+        (v, ds) <- act
+        modify $ \(s,done) ->
+            let t = Trace key [(d, getHash d s) | d <- ds] (getHash key s)
+                ts = Traces (t : traces) (Map.insert (hash v) v contents)
+            in (putInfo ts (putValue key v s), done)
+    else do
+        s <- gets fst
+        when (getHash key s `notElem` poss) $
+            modify $ \(s, done) -> (putValue key (contents Map.! head poss) s, done)
