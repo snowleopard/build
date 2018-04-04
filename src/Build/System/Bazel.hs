73cfbbb002482b0f66624e6bdd291226f14631e4
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-04-04 21:48:19 +0100

    Rewrite Bazel using abstract constructive traces

--- src/Build/System/Bazel.hs
+++ src/Build/System/Bazel.hs
@@ -7,19 +7,17 @@ import Build.Algorithm
 import Build.Store
 import Build.Trace
 
-import qualified Data.Map as Map
-
-bazel :: (Ord k, Hashable v) => Build Applicative (Traces k v) k v
-bazel = topological $ \key ds act -> do
-    s <- get
-    let Traces traces contents = getInfo s
-    poss <- traceMatch (\k v -> return $ getHash k s == v) key traces
-    if null poss then do
-        v <- act
-        modify $ \s ->
-            let t = Trace key [(d, getHash d s) | d <- ds] (getHash key s)
-                ts = Traces (t : traces) (Map.insert (hash v) v contents)
-            in putInfo ts (putValue key v s)
-    else do
-        when (getHash key s `notElem` poss) $
-            modify $ putValue key (contents Map.! head poss)
+bazel :: (Ord k, Hashable v) => Build Applicative (CT k v) k v
+bazel = topological $ \key deps act -> do
+    ct <- gets getInfo
+    store <- get
+    dirty <- not <$> verifyCT (\k -> return $ getHash k store) key ct
+    when dirty $ do
+        maybeValue <- constructCT (\k -> return $ getHash k store) key ct
+        case maybeValue of
+            Just value -> modify (putValue key value)
+            Nothing -> do
+                value <- act
+                modify $ \s ->
+                    let newS = putValue key value s
+                    in updateInfo (recordCT newS key deps) newS
