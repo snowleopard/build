7f82f87130d3b3b9c527a07dc2736bf31926c2ca
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-04-21 21:17:31 +0100

    Revise selfTrackingM

--- src/Build/SelfTracking.hs
+++ src/Build/SelfTracking.hs
@@ -1,15 +1,18 @@
 {-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
--- | This module defines 3 different selfTracking strategies. It's all based around the idea of a
---   String (which can be stored) being able to be parsed into a Task.
+-- | This module defines 3 different strategies of self-tracking. It's all based
+-- around the idea of storing task descriptions that can be parsed into a Task.
 --
--- * For Monad it works out beautifully. You just store the rule on the disk, and depend on it.
+-- * For Monad it works out beautifully. You just store the rule on the disk,
+--   and depend on it.
 --
--- * For Applicative1, if you are willing to give up cutoff, you can just store the rules as a Tree
---   in the node itself. That works adequately, but in practice I don't think anyone does it like that.
+-- * For Applicative1, if you are willing to give up cutoff, you can just store
+--   the rules as a Tree in the node itself. That works adequately, but in
+--   practice I (Neil) don't think anyone does it like that.
 --
--- * For Applicative2, we generate a fresh Task each time, but have that Task depend on a fake version
---   of the rules. This is a change in the Task, but it's one for which the standard implementations
---   tend to cope with just fine. Most Applicative systems with tracing probably actually do it this way.
+-- * For Applicative2, we generate a fresh Task each time, but have that Task
+--   depend on a fake version of the rules. This is a change in the Task, but
+--   it's one for which the standard implementations tend to cope with just fine.
+--   Most Applicative systems with self-tracking probably do it this way.
 module Build.SelfTracking(
     KeyM (..), selfTrackingM,
     KeyA1 (..), selfTrackingA1,
@@ -21,20 +24,40 @@ import Data.Tree
 import Build.Task
 import Build.Task.Depend
 
+-- We assume that the fetch passed to a Task is consistent and returns values
+-- matching the keys. It is possible to switch to typed tasks to check this
+-- assumption at compile time, e.g. see "Build.Task.Typed".
 data KeyM k     = KeyM k   | KeyTaskM k
 data ValueM v t = ValueM v | ValueTaskM t
 
--- | A model using Monad, works beautifully and allows storing the key on the disk
-selfTrackingM :: (t -> Task Monad k v) -> Tasks Monad (KeyM k) (ValueM v t)
-selfTrackingM _     (KeyTaskM _) = Nothing -- the Tasks must be an input stored on disk
-selfTrackingM parse (KeyM     k) = Just $ Task $ \fetch -> do
-    ValueTaskM taskDescription <- fetch (KeyTaskM k)
-    ValueM <$> run (parse taskDescription) (fmap (\(ValueM v) -> v) . fetch . KeyM)
+-- Fetch a value
+fetchValueM :: Monad m => (KeyM k -> m (ValueM v t)) -> k -> m v
+fetchValueM fetch key = do
+    value <- fetch (KeyM key)
+    case value of ValueM v -> return v
+                  _ -> error "Inconsistent fetch"
+
+-- Fetch a task description
+fetchValueTaskM :: Monad m => (KeyM k -> m (ValueM v t)) -> k -> m t
+fetchValueTaskM fetch key = do
+    value <- fetch (KeyTaskM key)
+    case value of ValueTaskM t -> return t
+                  _ -> error "Inconsistent fetch"
+
+-- For simplicity our task description parsers are total.
+type TaskParser c k v t = t -> Task c k v
+
+-- A model using Monad, works beautifully and allows storing the key on the disk
+selfTrackingM :: TaskParser Monad k v t -> Tasks Monad (KeyM k) (ValueM v t)
+selfTrackingM _      (KeyTaskM _) = Nothing -- Task keys are inputs
+selfTrackingM parser (KeyM     k) = Just $ Task $ \fetch -> do
+    task <- parser <$> fetchValueTaskM fetch k -- Fetch and parse the task description
+    ValueM <$> run task (fetchValueM fetch)
 
 data KeyA1 k = KeyA1 k (Tree String)
 
--- | First Applicative model, requires every key to be able to associate with it's environment (e.g. a reader somewhere)
---   Does not support cutoff if a key changes
+-- | First Applicative model, requires every key to be able to associate with
+-- its environment (e.g. a reader somewhere). Does not support cutoff if a key changes
 selfTrackingA1 :: (String -> Maybe (Task Applicative k v)) -> Tasks Applicative (KeyA1 k) v
 selfTrackingA1 parse (KeyA1 _ (Node task children)) = case parse task of
     Nothing -> Nothing
