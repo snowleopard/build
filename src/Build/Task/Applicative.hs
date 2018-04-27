01b03ac49028d0a2c5b7582b2bb8d4215dde8612
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-04-27 11:33:46 +0100

    More stuff

--- src/Build/Task/Applicative.hs
+++ src/Build/Task/Applicative.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ConstraintKinds, DeriveFunctor, RankNTypes, ScopedTypeVariables #-}
 module Build.Task.Applicative (
     pureTask, dependencies, inputs, partial, exceptional
     ) where
@@ -6,6 +6,7 @@ module Build.Task.Applicative (
 import Control.Applicative
 import Data.Functor.Compose
 import Data.Maybe
+import Data.Proxy
 
 import Build.Task
 import Build.Utilities
@@ -13,6 +14,40 @@ import Build.Utilities
 -- | An applicative task that returns a constant.
 pureTask :: v -> Task Applicative k v
 pureTask v = Task $ const (pure v)
+
+type Ts c k v = k -> Maybe (TT c k v)
+newtype TT c k v = TT { t :: T c k v }
+type T c k v = forall f. c f => (k -> f v) -> f v
+
+tf :: T Functor Int Int
+tf fetch = (+1) <$> fetch 0
+
+ta :: T Applicative Int Int
+ta = tf
+
+ttf :: TT Functor Int Int
+ttf = TT tf
+
+tta :: TT Applicative Int Int
+tta = TT ta
+
+ds :: T Applicative k v -> [k]
+ds task = getConst $ task (\k -> Const [k])
+
+isi :: forall k v. Ts Applicative k v -> k -> Bool
+isi tasks = isNothing . tasks
+
+-- wT :: Tsc c k v -> a -> (Tc c k v -> a) -> k -> a
+-- wT tasks o i key = case tasks key of
+--     Nothing -> o
+--     Just t  -> i t
+
+is :: forall k v. Ord k => Ts Applicative k v -> k -> [k]
+is tasks = filter (isi tasks) . reachable (maybe [] d . tasks)
+  where
+    d :: TT Applicative k v -> [k]
+    d tt = ds (t tt)
+
 
 -- TODO: Does this always terminate? It's not obvious!
 dependencies :: Task Applicative k v -> [k]
