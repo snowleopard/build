5bccb2978fcae3a4fcd8a5f900f1aa97172ec55b
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-05-08 01:17:27 +0200

    Try to simplify typed tasks

--- src/Build/Task/Typed.hs
+++ src/Build/Task/Typed.hs
@@ -2,44 +2,32 @@
 {-# LANGUAGE DataKinds, GADTs, TypeFamilies, KindSignatures #-}
 {-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# OPTIONS_GHC -Wno-unticked-promoted-constructors #-}
-module Build.Task.Typed (TTask, T (..), Key (..), digits, dependencies, fetch) where
+module Build.Task.Typed (TTask, Key (..), digits, fetch) where
 
 import Data.Functor.Const
 import Data.Functor.Identity
 
 type family Value k :: *
 
-type TTask c k = forall f (t :: T). c f => (forall (t :: T). k t -> f (Value (k t))) -> k t -> Maybe (f (Value (k t)))
+type TTask c k = forall f. c f => (forall k. k -> f (Value k)) -> k -> Maybe (f (Value k))
 
-data T = S | P | L deriving (Eq, Show)
+data Key a where
+    Base       :: Key Int
+    Number     :: Key Int
+    SplitDigit :: Key (Int, Int)
+    LastDigit  :: Key Int
+    BaseDigits :: Key [Int]
 
-data Key t where
-    Base       :: Key S
-    Number     :: Key S
-    SplitDigit :: Key P
-    LastDigit  :: Key S
-    BaseDigits :: Key L
+type instance Value (Key a) = a
 
-instance Show (Key t) where
-    show key = case key of
-        Base       -> "Base"
-        Number     -> "Number"
-        SplitDigit -> "SplitDigit"
-        LastDigit  -> "LastDigit"
-        BaseDigits -> "BaseDigits"
-
-type instance Value (Key S) = Integer
-type instance Value (Key P) = (Integer, Integer)
-type instance Value (Key L) = [Integer]
-
-digits :: TTask Applicative Key
+digits :: TTask Applicative (Key a)
 digits fetch SplitDigit = Just $ divMod <$> fetch Number <*> fetch Base
 digits fetch LastDigit  = Just $ snd <$> fetch SplitDigit
 digits fetch BaseDigits = Just $ enumFromTo 1 <$> fetch Base
 digits _ _ = Nothing
 
-dependencies :: TTask Applicative Key -> Key t -> [String]
-dependencies task = maybe [] getConst . task (\k -> Const [show k])
+-- dependencies :: TTask Applicative (Key a) -> Key a -> [Key a]
+-- dependencies task = maybe [] getConst . task (\k -> Const [k])
 
 fetch :: Key t -> Identity (Value (Key t))
 fetch key = Identity $ case key of
