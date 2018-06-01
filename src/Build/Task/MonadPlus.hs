514cf53eff9ced1a8315be45c1e055c709856865
Andrey Mokhov <andrey.mokhov@gmail.com>
2018-06-01 13:07:01 +0100

    Simplify, add comments

--- src/Build/Task/MonadPlus.hs
+++ src/Build/Task/MonadPlus.hs
@@ -1,31 +1,28 @@
-{-# LANGUAGE FlexibleContexts, RankNTypes, ScopedTypeVariables, TypeApplications #-}
+{-# LANGUAGE RankNTypes, TypeApplications #-}
 
 -- | A version of Monad tasks with some support for non-determinism.
-module Build.Task.MonadPlus (
-    random, dependenciesM, unwrap, computeND, correctBuild
-    ) where
+module Build.Task.MonadPlus (random, computeND, correctBuildValue) where
 
 import Control.Monad
-import Control.Monad.Writer
 
 import Build.Task
 import Build.Task.Wrapped
 import Build.Store
 
+-- | An example of a non-deterministic task: generate a random number from a
+-- specified interval.
 random :: (Int, Int) -> Task MonadPlus k Int
 random (low, high) = const $ foldr mplus mzero $ map pure [low..high]
 
-dependenciesM :: MonadPlus m => Task MonadPlus k v -> (k -> m v) -> m [k]
-dependenciesM task store = execWriterT $ task fetch
-  where
-    fetch k = tell [k] >> lift (store k)
-
 -- | Run a non-deterministic task with a pure lookup function, listing all
--- possible results, including @Nothing@ indicating that a given key is an input.
+-- possible results.
 computeND :: Task MonadPlus k v -> (k -> v) -> [v]
 computeND task store = task (return . store)
 
-correctBuild :: Eq v => Tasks MonadPlus k v -> Store i k v -> Store i k v -> k -> Bool
-correctBuild tasks store result k = case tasks k of
+-- | Given a description of @tasks@, an initial @store@, and a @result@ produced
+-- by running a build system on a target @key@, this function returns 'True' if
+-- the @key@'s value is a possible result of running the associated task.
+correctBuildValue :: Eq v => Tasks MonadPlus k v -> Store i k v -> Store i k v -> k -> Bool
+correctBuildValue tasks store result k = case tasks k of
     Nothing -> getValue k result == getValue k store
     Just t -> getValue k result `elem` computeND (unwrap @MonadPlus t) (flip getValue store)
